e93e9ac5100aafe51138a6604baaac0941fb3772
Vitalii <vitaliimusiienko10@gmail.com>
2025-12-27 12:03:14 +0200

    add Node class for heap visualization

---
+++ heap_visualization/node.py
@@ -0,0 +1,9 @@
+import uuid
+
+class HeapNode:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color
+        self.id = str(uuid.uuid4())пе
